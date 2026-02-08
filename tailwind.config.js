3c04f92188cdfe6c6b2da0bfca6afa1b0c5dfce8
DavidFranco135 <davidbhmg147@gmail.com>
2026-02-08 02:07:22 -0300

    Add Tailwind CSS configuration file

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        'ouro': '#D4AF37',
+      }
+    },
+  },
+  plugins: [],
+}
