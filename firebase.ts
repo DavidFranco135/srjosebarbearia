76f49b31963fa58fda9d471a18d76f051aabd1d4
DavidFranco135 <davidbhmg147@gmail.com>
2026-02-11 20:01:50 -0300

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import { getFirestore, Firestore } from "firebase/firestore";
+import { getAuth, Auth } from "firebase/auth";
+import { FirebaseApp } from "firebase/app";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDx2p5tkMaeAjL6WrHNxFvK7rKZ77iFZ4w",
@@ -11,6 +12,15 @@ const firebaseConfig = {
   appId: "1:952138197288:web:11b60ce4a514d346ce081d"
 };
 
-export const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
-export const auth = getAuth(app);
+// Inicializar app
+const app: FirebaseApp = initializeApp(firebaseConfig);
+
+// Inicializar serviços
+const db: Firestore = getFirestore(app);
+const auth: Auth = getAuth(app);
+
+// Exportar individualmente
+export { app, db, auth };
+
+// Exportar também como default (algumas configurações precisam)
+export default { app, db, auth };
